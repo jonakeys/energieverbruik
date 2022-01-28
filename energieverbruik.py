73464d3de4561c5ea6ad87444d57b39c7705c29f
Jonathan van der Steege <jonakeys@hotmail.com>
2022-01-28 19:38:35 +0100

    Performance verbeterd door multiprocessing.

--- energieverbruik.py
+++ energieverbruik.py
@@ -12,6 +12,8 @@ import pandas as pd
 import matplotlib.pyplot as plot_gas
 import matplotlib.pyplot as plot_ele
 from matplotlib import rcParams
+from multiprocessing import Process
+
 
 MAAND = 0
 DAG_VAN_MAAND = 26
@@ -22,7 +24,6 @@ VERBRUIK_WATER = 7
 PRIJS_GAS = 1.43
 PRIJS_ELEKTRICITEIT = 0.47
 PRIJS_WATER = 1.021
-
 
 
 # Inlezen data graaddagen, verbruik voorgaande jaar en afgeronde maanden
@@ -198,83 +199,87 @@ verschil_elektriciteit = (schatting_totaal_elektriciteit -
                           sum_verbruik_elektriciteit_2021)
 verschil_water = (schatting_totaal_water - sum_verbruik_water_2021)
 
+
 #
 # WEERGEVEN DATA
 #
+def ToonOverzicht():
+    # Toon overzicht, verbruik 2021 en schatting 2022
+    schema_g = {"maand": df['maand'],
+                "vrbr_g_2021": totaal_verbruik_gas_2021,
+                "vrw_g_2022": schatting_verbruik_gas_2022,
+                "vrsch_gas": schatting_verbruik_gas_2022 - totaal_verbruik_gas_2021}
+    schema_e = {"maand": df['maand'],
+                "vrbr_e_2021": totaal_verbruik_ele_2021,
+                "vrw_e_2022": schatting_verbruik_elektriciteit_2022,
+                "vrsch_ele": (schatting_verbruik_elektriciteit_2022 -
+                              totaal_verbruik_ele_2021)}
+    schema_w = {"maand": df['maand'],
+                "vrbr_w_2021": totaal_verbruik_wat_2021,
+                "vrw_w_2022": schatting_verbruik_water_2022,
+                "vrsch_wat": (schatting_verbruik_water_2022 -
+                              totaal_verbruik_wat_2021)}
+    overzicht_g = pd.DataFrame(schema_g)
+    overzicht_e = pd.DataFrame(schema_e)
+    overzicht_w = pd.DataFrame(schema_w)
+    str_overzicht = "Gas\n{sch_g}\n\nElektriciteit\n{sch_e}\n\nWater\n{sch_w}\n\n".format(sch_g=overzicht_g,
+                    sch_e=overzicht_e,
+                    sch_w=overzicht_w)
+    # Schrijf overzicht naar bestand
+    f = open("energieverbruik_2022_overzicht.txt", "w")
+    f.write(str_overzicht)
+    f.close()
 
-# Toon overzicht, verbruik 2021 en schatting 2022
-schema_g = {"maand": df['maand'],
-            "vrbr_g_2021": totaal_verbruik_gas_2021,
-            "vrw_g_2022": schatting_verbruik_gas_2022,
-            "vrsch_gas": schatting_verbruik_gas_2022 - totaal_verbruik_gas_2021}
-schema_e = {"maand": df['maand'],
-            "vrbr_e_2021": totaal_verbruik_ele_2021,
-            "vrw_e_2022": schatting_verbruik_elektriciteit_2022,
-            "vrsch_ele": (schatting_verbruik_elektriciteit_2022 -
-                          totaal_verbruik_ele_2021)}
-schema_w = {"maand": df['maand'],
-            "vrbr_w_2021": totaal_verbruik_wat_2021,
-            "vrw_w_2022": schatting_verbruik_water_2022,
-            "vrsch_wat": (schatting_verbruik_water_2022 -
-                          totaal_verbruik_wat_2021)}
-overzicht_g = pd.DataFrame(schema_g)
-overzicht_e = pd.DataFrame(schema_e)
-overzicht_w = pd.DataFrame(schema_w)
-str_overzicht = "Gas\n{sch_g}\n\nElektriciteit\n{sch_e}\n\nWater\n{sch_w}\n\n".format(sch_g=overzicht_g,
-                                                sch_e=overzicht_e,
-                                                sch_w=overzicht_w)
 
-str_output = ("2021\n" +
-              ("\tVerbruik gas: %d m3\n" % sum_verbruik_gas_2021) +
-              ("\tVerbruik elektriciteit: %d kWh\n" % sum_verbruik_elektriciteit_2021) +
-              ("\tVerbruik water: %d m3\n" % sum_verbruik_water_2021) +
-              "2022\n" +
-              ("\tGeschat gas: %d m3\n" % (schatting_totaal_gas + vast_gas_jr)) +
-              ("\tGeschat elektriciteit: %d kWh\n" % schatting_totaal_elektriciteit) +
-              ("\tGeschat water: %d m3\n" % schatting_totaal_water) +
-              "Verschil verbruik\n" +
-              ("\tGas: %d m3 (EUR %d)\n" % (verschil_gas, (verschil_gas * PRIJS_GAS))) +
-              ("\tElektriciteit: %d kWh (EUR %d)\n" %
-               (verschil_elektriciteit, (verschil_elektriciteit * PRIJS_ELEKTRICITEIT))) +
-              ("\tWater: %d m3 (EUR %d)\n" % (verschil_water, (verschil_water * PRIJS_WATER))) +
-              ("\tTarieven: [gas %.2f / m3] [ele %.2f / kWh] [wat %.2f / m3]\n" %
-               (PRIJS_GAS, PRIJS_ELEKTRICITEIT, PRIJS_WATER)))
+def PrintOutput():
+    str_output = ("2021\n" +
+                  ("\tVerbruik gas: %d m3\n" % sum_verbruik_gas_2021) +
+                  ("\tVerbruik elektriciteit: %d kWh\n" % sum_verbruik_elektriciteit_2021) +
+                  ("\tVerbruik water: %d m3\n" % sum_verbruik_water_2021) +
+                  "2022\n" +
+                  ("\tGeschat gas: %d m3\n" % (schatting_totaal_gas + vast_gas_jr)) +
+                  ("\tGeschat elektriciteit: %d kWh\n" % schatting_totaal_elektriciteit) +
+                  ("\tGeschat water: %d m3\n" % schatting_totaal_water) +
+                  "Verschil verbruik\n" +
+                  ("\tGas: %d m3 (EUR %d)\n" % (verschil_gas, (verschil_gas * PRIJS_GAS))) +
+                  ("\tElektriciteit: %d kWh (EUR %d)\n" %
+                   (verschil_elektriciteit, (verschil_elektriciteit * PRIJS_ELEKTRICITEIT))) +
+                  ("\tWater: %d m3 (EUR %d)\n" % (verschil_water, (verschil_water * PRIJS_WATER))) +
+                  ("\tTarieven: [gas %.2f / m3] [ele %.2f / kWh] [wat %.2f / m3]\n" %
+                   (PRIJS_GAS, PRIJS_ELEKTRICITEIT, PRIJS_WATER)))
+    # Toon uitvoer overzicht en totalen
+    #print(str_overzicht)
+    print(str_output)
 
-str_csv_data = ("vrbr_2021,sch_2022,vrsch_vrbr,vrsch_bedr,tar\n" +
-                str("%d" % sum_verbruik_gas_2021) +
-                str(",%d" % (schatting_totaal_gas + vast_gas_jr)) +
-                str(",%d" % verschil_gas) +
-                str(",%d" % (verschil_gas * PRIJS_GAS)) +
-                str(",%.2f\n" % PRIJS_GAS) +
-                str("%d" % sum_verbruik_elektriciteit_2021) +
-                str(",%d" % schatting_totaal_elektriciteit) +
-                str(",%d" % verschil_elektriciteit) +
-                str(",%d" % (verschil_elektriciteit * PRIJS_ELEKTRICITEIT)) +
-                str(",%.2f\n" % PRIJS_ELEKTRICITEIT) +
-                str("%d" % sum_verbruik_water_2021) +
-                str(",%d" % schatting_totaal_water) +
-                str(",%d" % verschil_water) +
-                str(",%d" % (verschil_water * PRIJS_WATER)) +
-                str(",%.2f\n" % PRIJS_WATER))
+    # Schrijf uitvoer naar bestand
+    f = open("energieverbruik_2022.txt", "w")
+    f.write(str_output)
+    f.close()
 
-# Toon uitvoer overzicht en totalen
-#print(str_overzicht)
-print(str_output)
 
-# Schrijf uitvoer naar bestand
-f = open("energieverbruik_2022.txt", "w")
-f.write(str_output)
-f.close()
+def SchrijfCsvData():
+    str_csv_data = ("vrbr_2021,sch_2022,vrsch_vrbr,vrsch_bedr,tar\n" +
+                    str("%d" % sum_verbruik_gas_2021) +
+                    str(",%d" % (schatting_totaal_gas + vast_gas_jr)) +
+                    str(",%d" % verschil_gas) +
+                    str(",%d" % (verschil_gas * PRIJS_GAS)) +
+                    str(",%.2f\n" % PRIJS_GAS) +
+                    str("%d" % sum_verbruik_elektriciteit_2021) +
+                    str(",%d" % schatting_totaal_elektriciteit) +
+                    str(",%d" % verschil_elektriciteit) +
+                    str(",%d" % (verschil_elektriciteit * PRIJS_ELEKTRICITEIT)) +
+                    str(",%.2f\n" % PRIJS_ELEKTRICITEIT) +
+                    str("%d" % sum_verbruik_water_2021) +
+                    str(",%d" % schatting_totaal_water) +
+                    str(",%d" % verschil_water) +
+                    str(",%d" % (verschil_water * PRIJS_WATER)) +
+                    str(",%.2f\n" % PRIJS_WATER))
 
-# Schrijf overzicht naar bestand
-f = open("energieverbruik_2022_overzicht.txt", "w")
-f.write(str_overzicht)
-f.close()
+    # Schrijf csv-data naar bestand
+    f = open("energie_hubrpi.csv", "w")
+    f.write(str_csv_data)
+    f.close()
 
-# Schrijf csv-data naar bestand
-f = open("energie_hubrpi.csv", "w")
-f.write(str_csv_data)
-f.close()
 
 # Toon grafieken
 maanden = ["jan", "feb", "mrt", "apr", "mei", "jun", "jul", "aug", "sep",
@@ -282,44 +287,76 @@ maanden = ["jan", "feb", "mrt", "apr", "mei", "jun", "jul", "aug", "sep",
 rcParams['axes.edgecolor'] = 'White'
 rcParams['figure.figsize'] = [7.8, 4.2]
 
-# Grafiek gasverbruik
-plot_gas.figure()
-plot_gas.plot(maanden, schatting_verbruik_gas_2022, color='tab:orange', label="2022", linewidth=5)
-plot_gas.plot(maanden, df['vrbr_gas_2021'], color='tab:blue', label="2021", linewidth=5)
-plot_gas.title("Gas", color='White')
-plot_gas.xlabel("maand", color='White')
-plot_gas.ylabel("verbruik (in m3)", color='White')
-plot_gas.tick_params(colors='White')
-plot_gas.grid()
-plot_gas.legend()
-plot_gas.tight_layout()
-plot_gas.savefig('gasverbruik.png', dpi=100, transparent=True)
-plot_gas.show()
 
-# Grafiek elektriciteitsverbruik
-plot_ele.figure()
-plot_ele.plot(maanden, schatting_verbruik_elektriciteit_2022, color='tab:orange', label="2022", linewidth=5)
-plot_ele.plot(maanden, df['vrbr_ele_2021'], color='tab:blue', label="2021", linewidth=5)
-plot_ele.title("Elektriciteit", color='White')
-plot_ele.xlabel("maand", color='White')
-plot_ele.ylabel("verbruik (in kWh)", color='White')
-plot_ele.tick_params(colors='White')
-plot_ele.grid()
-plot_ele.legend()
-plot_ele.tight_layout()
-plot_ele.savefig('elektriciteitsverbruik.png', dpi=100, transparent=True)
-plot_ele.show()
+def GrafiekGas():
+    # Grafiek gasverbruik
+    plot_gas.figure()
+    plot_gas.plot(maanden, schatting_verbruik_gas_2022, color='tab:orange',
+                  label="2022", linewidth=5)
+    plot_gas.plot(maanden, df['vrbr_gas_2021'], color='tab:blue', label="2021",
+                  linewidth=5)
+    plot_gas.title("Gas", color='White')
+    plot_gas.xlabel("maand", color='White')
+    plot_gas.ylabel("verbruik (in m3)", color='White')
+    plot_gas.tick_params(colors='White')
+    plot_gas.grid()
+    plot_gas.legend()
+    plot_gas.tight_layout()
+    plot_gas.savefig('gasverbruik.png', dpi=100, transparent=True)
+    #plot_gas.show()
 
-# Grafiek waterverbruik
-plot_ele.figure()
-plot_ele.plot(maanden, schatting_verbruik_water_2022, color='tab:orange', label="2022", linewidth=5)
-plot_ele.plot(maanden, df['vrbr_wat_2021'], color='tab:blue', label="2021", linewidth=5)
-plot_ele.title("Water", color='White')
-plot_ele.xlabel("maand", color='White')
-plot_ele.ylabel("verbruik (in m3)", color='White')
-plot_ele.tick_params(colors='White')
-plot_ele.grid()
-plot_ele.legend()
-plot_ele.tight_layout()
-plot_ele.savefig('waterverbruik.png', dpi=100, transparent=True)
-plot_ele.show()
+
+def GrafiekElektriciteit():
+    # Grafiek elektriciteitsverbruik
+    plot_ele.figure()
+    plot_ele.plot(maanden, schatting_verbruik_elektriciteit_2022,
+                  color='tab:orange', label="2022", linewidth=5)
+    plot_ele.plot(maanden, df['vrbr_ele_2021'], color='tab:blue', label="2021",
+                  linewidth=5)
+    plot_ele.title("Elektriciteit", color='White')
+    plot_ele.xlabel("maand", color='White')
+    plot_ele.ylabel("verbruik (in kWh)", color='White')
+    plot_ele.tick_params(colors='White')
+    plot_ele.grid()
+    plot_ele.legend()
+    plot_ele.tight_layout()
+    plot_ele.savefig('elektriciteitsverbruik.png', dpi=100, transparent=True)
+    #plot_ele.show()
+
+
+def GrafiekWater():
+    # Grafiek waterverbruik
+    plot_ele.figure()
+    plot_ele.plot(maanden, schatting_verbruik_water_2022, color='tab:orange',
+                  label="2022", linewidth=5)
+    plot_ele.plot(maanden, df['vrbr_wat_2021'], color='tab:blue', label="2021",
+                  linewidth=5)
+    plot_ele.title("Water", color='White')
+    plot_ele.xlabel("maand", color='White')
+    plot_ele.ylabel("verbruik (in m3)", color='White')
+    plot_ele.tick_params(colors='White')
+    plot_ele.grid()
+    plot_ele.legend()
+    plot_ele.tight_layout()
+    plot_ele.savefig('waterverbruik.png', dpi=100, transparent=True)
+    #plot_ele.show()
+
+
+p1 = Process(target=ToonOverzicht)
+p2 = Process(target=PrintOutput)
+p3 = Process(target=SchrijfCsvData)
+p4 = Process(target=GrafiekGas)
+p5 = Process(target=GrafiekElektriciteit)
+p6 = Process(target=GrafiekWater)
+p1.start()
+p2.start()
+p3.start()
+p4.start()
+p5.start()
+p6.start()
+p1.join()
+p2.join()
+p3.join()
+p4.join()
+p5.join()
+p6.join()
